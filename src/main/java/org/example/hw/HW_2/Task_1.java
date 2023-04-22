0b3efac15085337c4e622a67d6fe581818d71ce5
pjanov <pjanov84@mail.ru>
2023-04-22 13:05:13 +0300

    Task_1

---
+++ src/main/java/org/example/hw/HW_2/Task_1.java
@@ -0,0 +1,34 @@
+package org.example.hw.HW_2;
+
+
+import java.util.Scanner;
+
+public class Task_1 {
+
+    // Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+    // и возвращает введенное значение. Ввод текста вместо числа не должно приводить к
+    // падению приложения, вместо этого, необходимо повторно запросить у пользователя ввод данных.
+
+    public static void main(String[] args) {
+        System.out.println(promptForFloat());
+    }
+
+    public static float promptForFloat() {
+        Scanner scanner = new Scanner(System.in);
+        boolean inputIsValid = false;
+        float result = 0;
+
+        while (!inputIsValid) {
+            try {
+                System.out.print("Введите дробное число: ");
+                result = scanner.nextFloat();
+                inputIsValid = true; // Ввод был корректным, закончить цикл
+            } catch (Exception e) {
+                System.out.println("Ошибка: некорректный ввод. Пожалуйста, повторите попытку.");
+                scanner.nextLine(); // Проигнорировать неправильный ввод и запросить новый ввод
+            }
+        }
+        scanner.close();
+        return result;
+    }
+}
